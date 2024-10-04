5d3ff8e0e31ce0ef5decf7a75d1fdb4ee7350d57
ShabenaQA <shabenaar@gmail.com>
2024-10-04 08:03:44 +0530

    All files

---
+++ src/Oct/ex_04102024/Calculator.java
@@ -0,0 +1,10 @@
+package Oct.ex_04102024;
+
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(10+20);
+        System.out.println(40-20);
+        System.out.println(10*20);
+        System.out.println(10%2);
+    }
+}
