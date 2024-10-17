32e9cbd2fc5730a85ce0f156602974fc53b9cd4e
ShabenaQA <shabenaar@gmail.com>
2024-10-17 07:51:35 +0530

    All Files 17 Oct

---
+++ src/Oct/ex_02102024/Lab003.java
@@ -0,0 +1,9 @@
+package Oct.ex_02102024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        System.out.println("Java Program 1");
+        System.out.println("Java Program 2");
+        System.out.println("Java Program 3");
+    }
+}
